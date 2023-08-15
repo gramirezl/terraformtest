eb1f5583a435232cfad6ce0cd426fb6bf0af1ea9
Gustavo Ramirez <gustavo.ramirez@axity.com>
2023-08-15 15:40:59 -0600

    cambio de nombre

--- Infraestructure/Provisioning/QA/storageaccount.tf
+++ Infraestructure/Provisioning/QA/storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sa" {
-  name                     = "oxxostorageaccount"
+  name                     = "oxxostorageaccountt"
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
