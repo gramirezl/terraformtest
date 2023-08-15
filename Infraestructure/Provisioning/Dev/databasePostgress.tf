37abd9d2d81f2ef211a2d6f68fa5a0fc5bb42119
Gustavo Ramirez <gustavo.ramirez@axity.com>
2023-08-15 16:16:04 -0600

    se cambia nombre

--- Infraestructure/Provisioning/Dev/databasePostgress.tf
+++ Infraestructure/Provisioning/Dev/databasePostgress.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_server" "postgresserver" {
-  name                = "oxxodbserverr"
+  name                = "oxxodbserver"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
